20f39eb983e570d900d6eab2b57d157dc7bfda26
Filippo Banno <fiban@microsoft.com>
2019-06-05 15:57:05 +0100

    Add initial interfaces stub
    Trying to follow Pitchfork layout
    (https://api.csswg.org/bikeshed/?force=1&url=https://raw.githubusercontent.com/vector-of-bool/pitchfork/develop/data/spec.bs#src).
    Build output is still in the default location, will fix later.

---
+++ libs/Microsoft.MixedReality.Sharing.Network/src/IChannel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+namespace Microsoft.MixedReality.Sharing.Network
+{
+    public interface IChannel
+    {
+    }
+}
