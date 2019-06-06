c538dc36f9992b74d76c922394490b9e7450c4dc
Filippo Banno <fiban@microsoft.com>
2019-06-06 14:03:11 +0100

    Fix license comment position

--- libs/Microsoft.MixedReality.Sharing.Network/src/IChannel.cs
+++ libs/Microsoft.MixedReality.Sharing.Network/src/IChannel.cs
@@ -1,8 +1,9 @@
-﻿using System;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
 using System.Collections.Generic;
 using System.Text;
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See LICENSE in the project root for license information.
 
 namespace Microsoft.MixedReality.Sharing.Network
 {
