5e31d10ab8db05974db00eeabf3b94d9a96b2caa
Filippo Banno <fiban@microsoft.com>
2019-07-09 15:32:35 +0100

    Add copyright notice

--- libs/Network/src/IChannelCategory.cs
+++ libs/Network/src/IChannelCategory.cs
@@ -1,6 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
 
 namespace Microsoft.MixedReality.Sharing.Network
 {
