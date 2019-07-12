286d795a0b10351f0d5e8daadfa9fbdc8eba737b
Filippo Banno <fiban@microsoft.com>
2019-07-12 16:31:29 +0100

    Address review comments on C API

--- libs/Interface-cpp/include/network.h
+++ libs/Interface-cpp/include/network.h
@@ -1,67 +1,112 @@
 #pragma once
 
+#include <cstdint>
 #include "export.h"
 
-typedef void* mrsEndpoint;
-typedef void* mrsChannelCategory;
-typedef void* mrsChannelQueue;
-typedef void* mrsMessage;
-typedef void* mrsChannel;
+struct mrsEndpoint;
+struct mrsChannelCategory;
+struct mrsChannelQueue;
+struct mrsMessage;
+struct mrsChannel;
 
-enum class mrsChannelType : unsigned char { UNORDERED, ORDERED };
+// 0 = success, otherwise failure
+using Result = int;
+
+enum class mrsChannelType : unsigned char { kUnordered, kOrdered };
 
 extern "C" {
 
-MRS_API void MRS_CALL mrsInit();
-MRS_API void MRS_CALL mrsQuit();
+MRS_API Result MRS_CALL mrsInit();
+MRS_API Result MRS_CALL mrsQuit();
 
 // IEndpoint
-MRS_API mrsEndpoint MRS_CALL mrsAcquireEndpoint(size_t id_size, const char* id);
-MRS_API void MRS_CALL mrsGetEndpointId(mrsEndpoint endpoint,
-                                       size_t* id_size_out,
-                                       const char** id_out);
-MRS_API mrsEndpoint MRS_CALL mrsReleaseEndpoint(mrsEndpoint);
+
+// Get a reference to an IEndpoint. Release with mrsReleaseEndpoint when
+// finished.
+MRS_API Result MRS_CALL mrsAcquireEndpoint(const char* id,
+                                           uint32_t id_size,
+                                           mrsEndpoint** endpoint_out);
+
+// Destroy with mrsDisposeString.
+MRS_API Result MRS_CALL mrsGetEndpointId(mrsEndpoint* endpoint,
+                                         const char** id_out,
+                                         uint32_t* id_size_out);
+
+MRS_API Result MRS_CALL mrsReleaseEndpoint(mrsEndpoint*);
 
 // IChannelCategory
-MRS_API mrsChannelCategory MRS_CALL mrsCreateCategory(size_t name_size,
-                                                      const char* name,
-                                                      mrsChannelType type);
-MRS_API void MRS_CALL mrsGetCategoryName(mrsChannelCategory category,
-                                         size_t* id_size_out,
-                                         const char** id_out);
-MRS_API mrsChannelType MRS_CALL mrsGetCategoryType(mrsChannelCategory category);
-MRS_API mrsChannelQueue MRS_CALL
-mrsGetCategoryQueue(mrsChannelCategory category);
-MRS_API void MRS_CALL mrsCategoryStartListening(mrsChannelCategory category);
-MRS_API void MRS_CALL mrsCategoryStopListening(mrsChannelCategory category);
-MRS_API void MRS_CALL mrsDisposeCategory(mrsChannelCategory category);
+
+// Destroy with mrsDisposeCategory.
+MRS_API Result MRS_CALL mrsCreateCategory(const char* name,
+                                          uint32_t name_size,
+                                          mrsChannelType type,
+                                          mrsChannelCategory** category_out);
+
+// Destroy with mrsDisposeString.
+MRS_API Result MRS_CALL mrsGetCategoryName(mrsChannelCategory* category,
+                                           const char** id_out,
+                                           uint32_t* id_size_out);
+
+MRS_API Result MRS_CALL mrsGetCategoryType(mrsChannelCategory* category,
+                                           mrsChannelType* type_out);
+
+// Stays alive until the category dies. No need to dispose.
+MRS_API Result MRS_CALL mrsGetCategoryQueue(mrsChannelCategory* category,
+                                            mrsChannelQueue** queue_out);
+
+MRS_API Result MRS_CALL mrsCategoryStartListening(mrsChannelCategory* category);
+
+MRS_API Result MRS_CALL mrsCategoryStopListening(mrsChannelCategory* category);
+
+MRS_API Result MRS_CALL mrsDisposeCategory(mrsChannelCategory* category);
 
 // IMessage
-MRS_API mrsChannelCategory MRS_CALL mrsGetMessageCategory(mrsMessage message);
-MRS_API mrsEndpoint MRS_CALL mrsGetMessageEndpoint(mrsMessage message);
-MRS_API void MRS_CALL mrsGetMessagePayload(mrsMessage message,
-                                           size_t* payload_size_out,
-                                           const char** payload_out);
-MRS_API void MRS_CALL mrsReleaseMessage(mrsMessage message);
+
+MRS_API Result MRS_CALL
+mrsGetMessageCategory(mrsMessage* message, mrsChannelCategory** category_out);
+
+MRS_API Result MRS_CALL mrsGetMessageEndpoint(mrsMessage* message,
+                                              mrsEndpoint* endpoint_out);
+
+MRS_API Result MRS_CALL mrsGetMessagePayload(mrsMessage* message,
+                                             const char** payload_out,
+                                             uint32_t* payload_size_out);
+
+MRS_API Result MRS_CALL mrsReleaseMessage(mrsMessage* message);
 
 // IMessageQueue
-MRS_API mrsMessage MRS_CALL mrsQueueTake(mrsChannelQueue queue);
-MRS_API bool MRS_CALL mrsQueueTryTake(mrsChannelQueue queue,
-                                      mrsMessage* messageOut);
+
+MRS_API Result MRS_CALL mrsQueueTake(mrsChannelQueue* queue,
+                                     mrsMessage** message_out);
+
+MRS_API Result MRS_CALL mrsQueueTryTake(mrsChannelQueue* queue,
+                                      mrsMessage** message_out);
 
 // IChannel
-MRS_API mrsChannel MRS_CALL mrsCreateChannel(mrsChannelCategory category,
-                                             mrsEndpoint endpoint);
-MRS_API mrsChannelCategory MRS_CALL mrsGetChannelCategory(mrsChannel channel);
-MRS_API mrsEndpoint MRS_CALL mrsGetChannelEndpoint(mrsChannel channel);
-MRS_API bool MRS_CALL mrsIsChannelOk(mrsChannel channel);
-MRS_API int MRS_CALL mrsGetChannelSendQueueCount(mrsChannel channel);
-MRS_API void MRS_CALL mrsChannelReconnect(mrsChannel channel);
-MRS_API void MRS_CALL mrsChannelSendMessage(mrsChannel channel,
-                                            size_t payload_size,
-                                            const char* payload);
-MRS_API void MRS_CALL mrsDestroyChannel(mrsChannel channel);
+
+// Destroy with mrsDisposeChannel.
+MRS_API Result MRS_CALL mrsCreateChannel(mrsChannelCategory category,
+                                         mrsEndpoint endpoint,
+                                         mrsChannel** channel_out);
+
+MRS_API Result MRS_CALL
+mrsGetChannelCategory(mrsChannel* channel, mrsChannelCategory** category_out);
+
+MRS_API Result MRS_CALL mrsGetChannelEndpoint(mrsChannel* channel,
+                                              mrsEndpoint** endpoint_out);
+
+MRS_API Result MRS_CALL mrsIsChannelOk(mrsChannel* channel, uint8_t* ok_out);
+
+MRS_API Result MRS_CALL mrsGetChannelSendQueueCount(mrsChannel* channel, uint32_t* count_out);
+
+MRS_API Result MRS_CALL mrsChannelReconnect(mrsChannel* channel);
+
+MRS_API Result MRS_CALL mrsChannelSendMessage(mrsChannel* channel,
+                                              const char* payload,
+                                              uint32_t payload_size);
+
+MRS_API Result MRS_CALL mrsDisposeChannel(mrsChannel channel);
 
 // Dispose strings returned by methods above.
-MRS_API void MRS_CALL mrsDisposeString(size_t size, const char* ptr);
+MRS_API Result MRS_CALL mrsDisposeString(uint32_t size, const char* ptr);
 }
