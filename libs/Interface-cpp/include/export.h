49b0f3adcbba292619fdf093f12b1943ddb85115
Filippo Banno <fiban@microsoft.com>
2019-07-12 15:54:18 +0100

    Use correct export defines

--- libs/Interface-cpp/include/export.h
+++ libs/Interface-cpp/include/export.h
@@ -3,10 +3,14 @@
 // P/Invoke uses stdcall by default. This can be changed, but Unity's IL2CPP
 // does not understand the CallingConvention attribute and instead
 // unconditionally forces stdcall. So use stdcall in the API to be compatible.
-#if defined(MR_SHARING_WIN)
+#if defined(_MSC_VER)
+#if defined(MR_SHARING_EXPORT)
 #define MRS_API __declspec(dllexport)
+#else
+#define MRS_API __declspec(dllimport)
+#endif
 #define MRS_CALL __stdcall
-#elif defined(MR_SHARING_ANDROID)
+#elif defined(__ANDROID__)
 #define MRS_API __attribute__((visibility("default")))
 #define MRS_CALL __attribute__((stdcall))
 #endif
