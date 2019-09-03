a7e658a65c573077710fd8cd03095617e7bf1ff8
Filippo Banno <fiban@microsoft.com>
2019-09-03 10:37:27 +0100

    Add option to join IP multicast group

--- libs/Matchmaking.Local/src/UdpPeerNetwork.cs
+++ libs/Matchmaking.Local/src/UdpPeerNetwork.cs
@@ -4,7 +4,9 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Net;
+using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using System.Threading;
 using System.Threading.Tasks;
@@ -24,19 +26,32 @@ namespace Microsoft.MixedReality.Sharing.Matchmaking
 
     public class UdpPeerNetwork : IPeerNetwork
     {
-        Socket socket_;
-        EndPoint broadcastEndpoint_;
-        EndPoint localEndpoint_;
-
+        private Socket socket_;
+        private readonly IPEndPoint broadcastEndpoint_;
+        private readonly IPEndPoint localEndpoint_;
+        private readonly JoinMulticastGroup joinMulticastGroup_;
         private readonly EndPoint anywhere_ = new IPEndPoint(IPAddress.Any, 0);
         private readonly byte[] readBuffer_ = new byte[1024];
         private readonly ArraySegment<byte> readSegment_;
 
-        public UdpPeerNetwork(EndPoint broadcast, EndPoint local)
+        public event Action<IPeerNetwork, IPeerNetworkMessage> Message;
+
+        public enum JoinMulticastGroup { DoNotJoin, Join }
+
+        /// <summary>
+        /// Create a new network.
+        /// </summary>
+        /// <param name="broadcast">Broadcast or multicast address used to send packets to other hosts.</param>
+        /// <param name="local">Local address that the network socket will bind to.</param>
+        /// <param name="joinMulticastGroup">
+        /// If <see cref="JoinMulticastGroup.Yes"/>, the network socket will join the multicast group corresponding to
+        /// `broadcast`; otherwise `broadcast` will be interpreted as a local broadcast address.</param>
+        public UdpPeerNetwork(IPEndPoint broadcast, IPEndPoint local, JoinMulticastGroup joinMulticastGroup = JoinMulticastGroup.DoNotJoin)
         {
-            readSegment_ = new ArraySegment<byte>(readBuffer_);
             broadcastEndpoint_ = broadcast;
             localEndpoint_ = local;
+            joinMulticastGroup_ = joinMulticastGroup;
+            readSegment_ = new ArraySegment<byte>(readBuffer_);
         }
 
         private void HandleAsyncRead(Task<SocketReceiveFromResult> task)
@@ -67,18 +82,52 @@ namespace Microsoft.MixedReality.Sharing.Matchmaking
             Debug.Assert(socket_ == null);
             socket_ = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
             socket_.Bind(localEndpoint_);
-            socket_.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, 1);
+
+            if (joinMulticastGroup_ == JoinMulticastGroup.Join)
+            {
+                if (broadcastEndpoint_.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    MulticastOption mcastOption;
+                    mcastOption = new MulticastOption(broadcastEndpoint_.Address, localEndpoint_.Address);
+
+                    socket_.SetSocketOption(SocketOptionLevel.IP,
+                                                SocketOptionName.AddMembership,
+                                                mcastOption);
+                }
+                else //< IPv6
+                {
+                    IPv6MulticastOption mcastOption;
+                    var ifaceIdx = GetIfaceIdxFromAddress(localEndpoint_.Address);
+                    mcastOption = new IPv6MulticastOption(broadcastEndpoint_.Address, ifaceIdx);
+
+                    socket_.SetSocketOption(SocketOptionLevel.IP,
+                                                SocketOptionName.AddMembership,
+                                                mcastOption);
+                }
+            }
+            else
+            {
+                // Assume this is a broadcast address.
+                socket_.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, 1);
+            }
 
             socket_.ReceiveFromAsync(readSegment_, SocketFlags.None, anywhere_)
                 .ContinueWith(HandleAsyncRead);
+        }
+
+        private static long GetIfaceIdxFromAddress(IPAddress localAddress)
+        {
+            var ifaces = NetworkInterface.GetAllNetworkInterfaces();
+            var found = ifaces.First(iface =>
+                        iface.GetIPProperties().UnicastAddresses.Any(
+                            addressInfo => addressInfo.Address.Equals(localAddress)));
+            return found.GetIPProperties().GetIPv6Properties().Index;
         }
 
         public void Stop()
         {
             socket_.Dispose();
         }
-
-        public event Action<IPeerNetwork, IPeerNetworkMessage> Message;
 
         public void Broadcast(byte[] msg)
         {
