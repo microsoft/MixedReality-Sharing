c22da668ef9b72670c00b085eab039f4888eb633
Filippo Banno <fiban@microsoft.com>
2019-09-27 17:04:00 +0100

    Add test for room timeout

--- libs/Matchmaking.Local/test/Matchmaking.Local.Test/SimpleTest.cs
+++ libs/Matchmaking.Local/test/Matchmaking.Local.Test/SimpleTest.cs
@@ -58,5 +58,44 @@ namespace Matchmaking.Local.Test
             var msg = System.Text.Encoding.UTF8.GetString(mem.ToArray());
             Assert.Contains("Large UDP", msg);
         }
+
+        [Fact]
+        public void RoomExpiresOnTime()
+        {
+            const int timeoutSec = 1;
+            var network1 = new MemoryPeerNetwork(1);
+            var network2 = new MemoryPeerNetwork(2);
+            using (var cts = new CancellationTokenSource(Utils.TestTimeoutMs))
+            using (var svc1 = new PeerMatchmakingService(network1))
+            {
+                const string category = "RoomExpiresOnTime";
+                var rooms1 = svc1.StartDiscovery(category);
+                Assert.Empty(rooms1.Rooms);
+
+                using (var svc2 = new PeerMatchmakingService(network2, new PeerMatchmakingService.Options { RoomExpirySec = timeoutSec }))
+                {
+                    // Create rooms from svc2
+                    var room1 = svc2.CreateRoomAsync(category, "conn1", null, cts.Token).Result;
+
+                    // It should show up in svc1
+                    {
+                        Task.Delay(timeoutSec * 1500).Wait();
+                        var res1 = Utils.QueryAndWaitForRoomsPredicate(svc1, category, rl => rl.Any(), cts.Token);
+                        Assert.Single(res1);
+                        Assert.Equal(room1.UniqueId, res1.First().UniqueId);
+                    }
+
+                    // Stop the network.
+                    network1.Stop();
+
+                    // Wait a bit after the timeout.
+                    Task.Delay(timeoutSec * 1500).Wait();
+                    {
+                        var res1 = Utils.QueryAndWaitForRoomsPredicate(svc1, category, rl => rl.Count() == 0, cts.Token);
+                        Assert.Empty(res1);
+                    }
+                }
+            }
+        }
     }
 }
