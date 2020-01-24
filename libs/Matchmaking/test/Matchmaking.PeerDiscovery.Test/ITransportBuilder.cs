e16ba393b4e709ff7f712e3cf1a09909b3fa5da7
Filippo Banno <fiban@microsoft.com>
2020-01-24 16:22:37 +0000

    Refactored transport tests into their own class
    Reduced a bit the number of equivalent tests run.

---
+++ libs/Matchmaking/test/Matchmaking.PeerDiscovery.Test/ITransportBuilder.cs
@@ -0,0 +1,206 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace Microsoft.MixedReality.Sharing.Matchmaking.Test
+{
+    // Utility for making multiple instances of IPeerDiscoveryTransport communicating with each other.
+    internal interface ITransportBuilder
+    {
+        IPeerDiscoveryTransport MakeTransport(int userIndex);
+        bool SimulatesPacketLoss { get; }
+    }
+
+    internal class MemoryTransportBuilder : ITransportBuilder
+    {
+        public IPeerDiscoveryTransport MakeTransport(int userIndex)
+        {
+            return new MemoryPeerDiscoveryTransport(userIndex);
+        }
+
+        public bool SimulatesPacketLoss => false;
+    }
+
+    internal class UdpTransportBuilder : ITransportBuilder
+    {
+        private readonly ushort port_ = Utils.NewPortNumber.Calculate();
+
+        public IPeerDiscoveryTransport MakeTransport(int userIndex)
+        {
+            return new UdpPeerDiscoveryTransport(new IPAddress(0xffffff7f), port_, new IPAddress(0x0000007f + (userIndex << 24)));
+        }
+        public bool SimulatesPacketLoss => false;
+    }
+
+    internal class UdpMulticastTransportBuilder : ITransportBuilder
+    {
+        private readonly ushort port_ = Utils.NewPortNumber.Calculate();
+
+        public IPeerDiscoveryTransport MakeTransport(int userIndex)
+        {
+            return new UdpPeerDiscoveryTransport(new IPAddress(0x000000e0), port_, new IPAddress(0x0000007f + (userIndex << 24)));
+        }
+        public bool SimulatesPacketLoss => false;
+    }
+
+    internal class UdpReorderedTransportBuilder : ITransportBuilder, IDisposable
+    {
+        public const int MaxDelayMs = 25;
+        public const int MaxRetries = 3;
+
+        private readonly Socket relay_;
+        private readonly ushort port_ = Utils.NewPortNumber.Calculate();
+        private readonly List<IPEndPoint> recipients_ = new List<IPEndPoint>();
+
+        private readonly Random random_;
+
+        // Wraps a packet for use in a map.
+        private class Packet
+        {
+            public IPEndPoint EndPoint;
+            public byte[] Contents;
+
+            public Packet(IPEndPoint endPoint, ArraySegment<byte> contents)
+            {
+                EndPoint = endPoint;
+                Contents = new byte[contents.Count];
+                for (int i = 0; i < contents.Count; ++i)
+                {
+                    Contents[i] = contents[i];
+                }
+            }
+
+            public override bool Equals(object other)
+            {
+                if (other is Packet rhs)
+                {
+                    return EndPoint.Equals(rhs.EndPoint) && Contents.SequenceEqual(rhs.Contents);
+                }
+                return false;
+            }
+
+            public override int GetHashCode()
+            {
+                // Not a great hash but it shouldn't matter in this case.
+                var result = 0;
+                foreach (byte b in Contents)
+                {
+                    result = (result * 31) ^ b;
+                }
+                return EndPoint.GetHashCode() ^ result;
+            }
+        }
+
+        // Keeps track of each packet that goes through the relay and counts its repetitions.
+        // See receive loop for usage.
+        private readonly Dictionary<Packet, int> packetCounters;
+
+        public bool SimulatesPacketLoss => (packetCounters != null);
+        public IPeerDiscoveryTransport MakeTransport(int userIndex)
+        {
+            // Peers all send packets to the relay.
+            var address = new IPAddress(0x0000007f + (userIndex << 24));
+            lock (recipients_)
+            {
+                var endpoint = new IPEndPoint(address, port_);
+
+                // Remove first so the same agent can be re-created multiple times
+                recipients_.Remove(endpoint);
+                recipients_.Add(endpoint);
+            }
+            return new UdpPeerDiscoveryTransport(new IPAddress(0xfeffff7f), port_, address,
+                new UdpPeerDiscoveryTransport.Options { MaxRetries = MaxRetries, MaxRetryDelayMs = 100 });
+        }
+
+        public UdpReorderedTransportBuilder(Random random, bool packetLoss)
+        {
+            random_ = random;
+
+            relay_ = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            relay_.Bind(new IPEndPoint(new IPAddress(0xfeffff7f), port_));
+
+            // Disable exception on UDP connection reset (don't care).
+            uint IOC_IN = 0x80000000;
+            uint IOC_VENDOR = 0x18000000;
+            uint SIO_UDP_CONNRESET = IOC_IN | IOC_VENDOR | 12;
+            relay_.IOControl((int)SIO_UDP_CONNRESET, new byte[] { Convert.ToByte(false) }, null);
+
+            if (packetLoss)
+            {
+                packetCounters = new Dictionary<Packet, int>();
+            }
+
+            Task.Run(async () =>
+            {
+                try
+                {
+                    while (true)
+                    {
+                        byte[] buf_ = new byte[1024];
+                        var result = await relay_.ReceiveFromAsync(new ArraySegment<byte>(buf_), SocketFlags.None, new IPEndPoint(IPAddress.Any, 0));
+
+                        if (packetCounters != null)
+                        {
+                            // Increase the probability of delivery of a packet with retries, up to 100% on the last retry.
+                            // This simulates heavy packet loss while still guaranteeing that everything works.
+                            // Note that this logic is very naive, but should be fine for small tests.
+                            var packet = new Packet((IPEndPoint)result.RemoteEndPoint, new ArraySegment<byte>(buf_, 0, result.ReceivedBytes));
+                            if (!packetCounters.TryGetValue(packet, out int counter))
+                            {
+                                counter = 0;
+                            }
+
+                            if (counter == MaxRetries - 1)
+                            {
+                                // Last retry, always send and forget the packet.
+                                packetCounters.Remove(packet);
+                            }
+                            else
+                            {
+                                packetCounters[packet] = counter + 1;
+                                // Drop with decreasing probability.
+                                if (random_.Next(0, MaxRetries) > counter)
+                                {
+                                    continue;
+                                }
+                            }
+                        }
+
+                        // The relay sends the packets to all peers with a random delay.
+                        IPEndPoint[] curRecipients;
+                        lock (recipients_)
+                        {
+                            curRecipients = recipients_.ToArray();
+                        }
+                        foreach (var rec in curRecipients)
+                        {
+                            var delay = random_.Next(MaxDelayMs);
+                            _ = Task.Delay(delay).ContinueWith(t =>
+                            {
+                                try
+                                {
+                                    relay_.SendToAsync(new ArraySegment<byte>(buf_, 0, result.ReceivedBytes), SocketFlags.None, rec);
+                                }
+                                catch (ObjectDisposedException) { }
+                                catch (SocketException e) when (e.SocketErrorCode == SocketError.NotSocket) { }
+                            });
+                        }
+                    }
+                }
+                catch (ObjectDisposedException) { }
+                catch (SocketException e) when (e.SocketErrorCode == SocketError.NotSocket) { }
+            });
+        }
+
+        public void Dispose()
+        {
+            relay_.Dispose();
+        }
+    }
+}
---
+++ libs/Matchmaking/test/Matchmaking.PeerDiscovery.Test/TransportTest.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Microsoft.MixedReality.Sharing.Matchmaking.Test
+{
+    public class TransportTest
+    {
+        private readonly Random random_;
+
+        public TransportTest(ITestOutputHelper output)
+        {
+            var seed = new Random().Next();
+            output.WriteLine($"Seed for lossy network: {seed}");
+            random_ = new Random(seed);
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void TestMemory(bool useMultiThread) { SendReceive(new MemoryTransportBuilder(), useMultiThread); }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void TestUdp(bool useMultiThread) { SendReceive(new UdpTransportBuilder(), useMultiThread); }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void TestUdpMulticast(bool useMultiThread) { SendReceive(new UdpMulticastTransportBuilder(), useMultiThread); }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void TestUdpReordered(bool useMultiThread)
+        {
+            SendReceive(new UdpReorderedTransportBuilder(random_, false), useMultiThread);
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void TestUdpReorderedUnreliable(bool useMultiThread)
+        {
+            SendReceive(new UdpReorderedTransportBuilder(random_, true), useMultiThread);
+        }
+
+        private void SendReceive(ITransportBuilder transportBuilder, bool useMultiThread)
+        {
+            using(var cts = new CancellationTokenSource(Utils.TestTimeoutMs))
+            {
+                const int lastId = 50;
+                const int guidsPerTransport = 3;
+
+                // Make a few transports in the same broadcast domain.
+                var transports = new IPeerDiscoveryTransport[5];
+
+                // Associate a few stream IDs to each transport.
+                int streamsNum = guidsPerTransport * transports.Length;
+                var streams = new Guid[streamsNum];
+
+                // Keep track of the messages received by each transport.
+                var lastIdFromGuid = new Dictionary<Guid, int>[transports.Length];
+                var lastMessageReceived = new Dictionary<Guid, ManualResetEvent>[transports.Length];
+
+
+                for (int transportIdx = 0; transportIdx < transports.Length; ++transportIdx)
+                {
+                    // Initialize transport and associated data.
+                    lastMessageReceived[transportIdx] = new Dictionary<Guid, ManualResetEvent>();
+                    lastIdFromGuid[transportIdx] = new Dictionary<Guid, int>();
+                    for (int guidIdx = 0; guidIdx < guidsPerTransport; ++guidIdx)
+                    {
+                        var streamId = Guid.NewGuid();
+                        streams[transportIdx * guidsPerTransport + guidIdx] = streamId;
+                        lastMessageReceived[transportIdx][streamId] = new ManualResetEvent(false);
+                    }
+                    transports[transportIdx] = transportBuilder.MakeTransport(transportIdx + 1);
+
+                    // Handle received messages.
+                    // Note: must copy into local variable or the lambda will capture the wrong value.
+                    int captureTransportIdx = transportIdx;
+                    transports[transportIdx].Message +=
+                        (IPeerDiscoveryTransport _, IPeerDiscoveryMessage message) =>
+                        {
+                            // Check that messages are received in send order.
+                            lastIdFromGuid[captureTransportIdx].TryGetValue(message.StreamId, out int lastReceivedId);
+                            int currentId = BitConverter.ToInt32(message.Contents);
+                            Assert.True(currentId > lastReceivedId);
+
+                            // Notify that the last message hasn't been dropped.
+                            if (currentId == lastId)
+                            {
+                                lastMessageReceived[captureTransportIdx][message.StreamId].Set();
+                            }
+                        };
+                    transports[transportIdx].Start();
+                }
+
+                try
+                {
+                    // Send a sequence of messages from every transport for every stream.
+                    var sendTasks = new List<Task>();
+                    for (int transportIdx = 0; transportIdx < transports.Length; ++transportIdx)
+                    {
+                        for (int guidIdx = 0; guidIdx < guidsPerTransport; ++guidIdx)
+                        {
+                            var guid = streams[transportIdx * guidsPerTransport + guidIdx];
+                            var transport = transports[transportIdx];
+                            Action sendMessages = () =>
+                            {
+                                for (int msgIdx = 1; msgIdx <= lastId; ++msgIdx)
+                                {
+                                    byte[] bytes = BitConverter.GetBytes(msgIdx);
+                                    transport.Broadcast(guid, bytes);
+                                }
+                            };
+                            if (useMultiThread)
+                            {
+                                sendTasks.Add(Task.Run(sendMessages));
+                            }
+                            else
+                            {
+                                sendMessages();
+                            }
+                        }
+                    }
+
+                    // Wait until all messages have been sent.
+                    Task.WaitAll(sendTasks.ToArray(), cts.Token);
+
+                    // The last message has been received for every stream by every transport.
+                    var allHandles = new List<WaitHandle>(streamsNum);
+                    foreach (var transportEvents in lastMessageReceived)
+                    {
+                        foreach(var ev in transportEvents.Values)
+                        {
+                            allHandles.Add(ev);
+                        }
+                    }
+                    // Workaround to WaitHandle.WaitAll not taking a CancellationToken.
+                    Task.Run(() => WaitHandle.WaitAll(allHandles.ToArray())).Wait(cts.Token);
+                }
+                finally
+                {
+                    Exception anyException = null;
+                    foreach (var transport in transports)
+                    {
+                        try
+                        {
+                            transport.Stop();
+                        }
+                        catch (Exception e)
+                        {
+                            anyException = e;
+                        }
+                    }
+                    if (anyException != null)
+                    {
+                        throw anyException;
+                    }
+                }
+            }
+        }
+    }
+}
